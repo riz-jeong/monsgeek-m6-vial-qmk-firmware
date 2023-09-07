7189b75d7bf9d5dfabd7a25f217f63bd5133941b
riz.dev <rizt.dev@gmail.com>
2023-09-08 00:15:20 +0900

    add Polling Rate, DEBOUNCE, MOUSEKEY Option

--- monsgeek/m6/keymaps/vial/config.h
+++ monsgeek/m6/keymaps/vial/config.h
@@ -4,3 +4,17 @@
 
 //Vial Keyboard UID
 #define VIAL_KEYBOARD_UID {0xCB, 0xDE, 0x1E, 0x06, 0xFD, 0x19, 0x48, 0xE7}
+
+/* Change USB Polling Rate to 1000hz and a larger keys per scan for elite gaming*/
+#define USB_POLLING_INTERVAL_MS 1
+#define QMK_KEYS_PER_SCAN 12
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 2
+
+/* Mouse Key Options */
+#define MOUSEKEY_INTERVAL 15
+#define MOUSEKEY_DELAY 20
+#define MOUSEKEY_TIME_TO_MAX 30
+#define MOUSEKEY_MAX_SPEED 3
+#define MOUSEKEY_WHEEL_DELAY 0
